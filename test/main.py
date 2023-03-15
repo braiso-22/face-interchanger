4669d3ff9639f0087f6047fc04f2d7deda777946
Brais <braisfv22@gmail.com>
2023-03-15 18:57:42 +0100

    more tests and model

--- test/main.py
+++ test/main.py
@@ -1,11 +1,16 @@
 import unittest
 
+import dlib
 import numpy as np
 
 from src import main
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.detector, _ = main.get_face_detector_and_predictor()
+        self.face1 = main.load_image("wismichu.jpg")
 
     def test_load_image_returns_ndarray(self):
         image = type(main.load_image("rajoy.jpg"))
@@ -14,6 +19,11 @@ class MyTestCase(unittest.TestCase):
     def test_load_image_returns_none_without_str(self):
         image = main.load_image("")
         self.assertTrue(image is None)
+
+    def test_get_face_detector_returns_detector_and_predictor(self):
+        detector, predictor = main.get_face_detector_and_predictor()
+        self.assertTrue(type(detector) is dlib.fhog_object_detector)
+        self.assertTrue(type(predictor) is dlib.shape_predictor)
 
 
 if __name__ == '__main__':
